85096882f013e16ce613bf2aa6bab3f684f1f136
IgnacyBerent <2gb02ignac@gmail.com>
2023-12-01 16:04:13 +0100

    day 1 solutions

---
+++ day_1/1/main.py
@@ -0,0 +1,21 @@
+def main():
+    with open("../input.txt") as file:
+        lines = file.readlines()
+        all_numbers = list(map(find_numbers, lines))
+        print(sum(all_numbers))
+
+
+def find_numbers(line: str) -> int:
+    numbers = list(filter(lambda x: x.isdigit(), line))
+    if len(numbers) == 1:
+        return make_whole_number(numbers[0], numbers[0])
+    else:
+        return make_whole_number(numbers[0], numbers[-1])
+
+
+def make_whole_number(x: int, y: int) -> int:
+    return int(str(x) + str(y))
+
+
+if __name__ == "__main__":
+    main()
---
+++ day_1/2/main.py
@@ -0,0 +1,51 @@
+nums_word = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+}
+
+
+def main():
+    with open("../input.txt") as file:
+        lines = file.readlines()
+        all_numbers = list(map(find_numbers, lines))
+        print(sum(all_numbers))
+
+
+def find_numbers(line: str) -> int:
+    numbers = []
+    for i, char in enumerate(line):
+        if char.isdigit():
+            numbers.append(int(char))
+        elif char.isalpha():
+            word = ''
+            for j in range(6):
+                try:
+                    line[i + j]
+                except IndexError:
+                    break
+                if line[i + j].isalpha():
+                    word += line[i + j]
+                else:
+                    break
+                if word in nums_word:
+                    numbers.append(nums_word[word])
+                    break
+    return make_whole_number(numbers)
+
+
+def make_whole_number(numbers: list) -> int:
+    if len(numbers) == 1:
+        return int(str(numbers[0]) + str(numbers[0]))
+    else:
+        return int(str(numbers[0]) + str(numbers[-1]))
+
+
+if __name__ == "__main__":
+    main()
