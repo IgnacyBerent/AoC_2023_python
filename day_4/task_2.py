543b10ad38bc9390094dcbcb5762728b1ca7f48f
Ignacy Berent <2gb02ignac@gmail.com>
2023-12-04 08:23:09 +0100

    code optimization

--- day_4/task_2.py
+++ day_4/task_2.py
@@ -6,8 +6,12 @@ def main():
         for line in lines:
             card_card_number, scratches = line.strip().split(":")
             _, card_number = card_card_number.strip().split()
-            list_of_cards.append((int(card_number), scratches))
-            cards_dict = {int(card_number): scratches for card_number, scratches in list_of_cards}
+            winning_scratch, my_scratch = scratches.strip().split("|")
+            winning_scratch = [int(scratch) for scratch in winning_scratch.strip().split()]
+            my_scratch = [int(card) for card in my_scratch.strip().split()]
+            scratches_tuple = (winning_scratch, my_scratch)
+            list_of_cards.append((int(card_number), scratches_tuple))
+            cards_dict = {int(card_number): tuple for card_number, tuple in list_of_cards}
 
         i = 0
         while i < len(list_of_cards):
@@ -22,13 +26,10 @@ def main():
         print(len(list_of_cards))
 
 
-def calculate_matches(scratches) -> int:
-    winning_scratch, my_scratch = scratches.strip().split("|")
-    winning_scratch = [int(scratch) for scratch in winning_scratch.strip().split()]
-    my_scratch = [int(card) for card in my_scratch.strip().split()]
+def calculate_matches(scratches: tuple[list, list]) -> int:
     matches = 0
-    for scratch in my_scratch:
-        if scratch in winning_scratch:
+    for scratch in scratches[0]:
+        if scratch in scratches[1]:
             matches += 1
     return matches
 
