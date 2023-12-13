8f0d635075df8ec0961d3b08b97c81a8d4bf2fb7
Ignacy Berent <2gb02ignac@gmail.com>
2023-12-13 01:10:14 +0100

    Successfully finishing task 2

--- day_12/task_2.py
+++ day_12/task_2.py
@@ -1,70 +1,83 @@
-from itertools import combinations
+from functools import cache
 
-FOLDS = 1
+FOLDS = 5
 
 
 def main():
-    with open('example.txt', 'r') as f:
+    with open('input.txt', 'r') as f:
         lines = f.readlines()
         all_poss = 0
-        for line in lines:
+        for j, line in enumerate(lines):
             record, nums = line.strip().split(' ')
             record_copy = record
             for i in range(FOLDS - 1):
                 record += '?'
                 record += record_copy
-            record += '.'
-            record = '.' + record
             nums = [int(num) for num in nums.split(',')]
             nums_copy = nums.copy()
             hashes = record.count('#')
             for i in range(FOLDS - 1):
                 nums += nums_copy
-            all_poss += calculate_poss_r(record, nums, hashes)
+
+            poss = calculate_poss_r(record=record,nums=tuple(nums),total_hashes=hashes)
+            all_poss += poss
     print(all_poss)
 
 
-def calculate_poss_r(record: str, nums: list[int], total_hashes, used_h=0) -> int:
+@cache
+def calculate_poss_r(record: str, nums: list[int], total_hashes, used_h=0, place_left = None) -> int:
+    if not record:
+        return 0
+
     t_hashes = total_hashes
     curr_num = nums[0]
-    place_ned = sum(nums) + len(nums) - 1
+    place_ned = sum(nums) + len(nums) -2
     place_left = record.count('#') + record.count('?') + record.count('.?') + record.count('.#') - place_ned
     if record[0] == '.':
         place_left -= 1
-    if not nums[1:]:
-        place_left += 1
 
     u_hashes = []
     shifts = []
     possib = 0
-    for i, char in enumerate(record):
+
+    if not nums[1:]:
+        r_l = record
+    else:
+        r_l = record[:-curr_num]
+
+    if curr_num > len(record):
+        return 0
+
+    for i, char in enumerate(r_l):
         if char != '.':
+            place_left -= 1
             if not nums[1:]:
-                try:
-                    if all(x != '.' for x in record[i:curr_num+i]) and record[i-1] != '#' and record[i+curr_num+1] != '#':
-                        if used_h + record[i:curr_num+i].count('#') == t_hashes:
-                            possib += 1
-                            place_left -= 1
-                            if place_left <= 0:
-                                break
-                except IndexError:
-                    break
-            else:
-                try:
-                    if all(x != '.' for x in record[i:curr_num+i]) and record[i-1] != '#' and record[i+curr_num+1] != '#':
+                if all(x != '.' for x in record[i:i+curr_num]):
+                    if used_h + record[i:i+curr_num].count('#') == t_hashes and len(record[i:i+curr_num]) == curr_num:
                         possib += 1
-                        place_left -= 1
+            else:
+                if i == 0:
+                    if all(x != '.' for x in record[i:i+curr_num]) and record[i+curr_num] != '#':
+                        possib += 1
                         u_hashes.append(record[i:curr_num+i].count('#'))
                         shifts.append(curr_num+i+1)
-                        if place_left <= 0:
-                            break
-                except IndexError:
-                    break
-    print(record, nums, possib)
+                elif i == len(record) - curr_num:
+                    if all(x != '.' for x in record[i:i+curr_num]) and record[i-1] != '#':
+                        possib += 1
+                        u_hashes.append(record[i:curr_num+i].count('#'))
+                        shifts.append(curr_num+i+1)
+                else:
+                    if all(x != '.' for x in record[i:i+curr_num]) and record[i-1] != '#' and record[i+curr_num] != '#':
+                        possib += 1
+                        u_hashes.append(record[i:curr_num+i].count('#'))
+                        shifts.append(curr_num+i+1)
+            if place_left == 0:
+                break
     if not nums[1:]:
         return possib
     else:
-        return sum([calculate_poss_r(record[shift:], nums[1:], t_hashes, hashes) for shift, hashes in zip(shifts, u_hashes)])
+        nums = tuple(list(nums)[1:])
+        return sum([calculate_poss_r(record[shift:], nums, t_hashes, hashes+used_h, place_left) for shift, hashes in zip(shifts, u_hashes)])
 
 
 if __name__ == '__main__':
